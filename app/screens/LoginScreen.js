52cf18cb6115db783166c672b0cf85c890781593
oussama <oussama.aouini@ensi-uma.tn>
2021-04-25 15:07:34 +0100

    installed Yup and added email & password  validation to LoginScreen

--- app/screens/LoginScreen.js
+++ app/screens/LoginScreen.js
@@ -1,10 +1,17 @@
 import React from "react";
 import { Image, StyleSheet } from "react-native";
 import AppTextInput from "../components/AppTextInput";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 import Screen from "../components/Screen";
 import AppButton from "../components/AppButton";
-import { Formik } from "formik";
+import AppText from "../components/AppText";
+
+const validationSchema = Yup.object().shape({
+  email: Yup.string().required().email().label("Email"),
+  password: Yup.string().required().min(4).label("Password"),
+});
 
 function LoginScreen(props) {
   return (
@@ -12,8 +19,9 @@ function LoginScreen(props) {
       <Formik
         initialValues={{ email: "", password: "" }}
         onSubmit={(values) => console.log(values)}
+        validationSchema={validationSchema}
       >
-        {({ handleChange, handleSubmit }) => (
+        {({ handleChange, handleSubmit, errors }) => (
           <>
             <Image
               resizeMode="contain"
@@ -29,6 +37,7 @@ function LoginScreen(props) {
               placeholder="Email"
               textContentType="emailAddress"
             />
+            <AppText style={{ color: "red" }}>{errors.email}</AppText>
             <AppTextInput
               autoCapitalize="none"
               autoCorrect={false}
@@ -38,6 +47,7 @@ function LoginScreen(props) {
               secureTextEntry
               textContentType="password"
             />
+            <AppText style={{ color: "red" }}>{errors.password}</AppText>
             <AppButton title="Login" onPress={handleSubmit} />
           </>
         )}
